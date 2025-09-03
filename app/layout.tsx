cc29826d23af2fef0dbf605dbdc8e7285c0129b1
Marcos Portales <marcosportales2002@gmail.com>
2025-09-03 14:27:58 +0200

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { AuthProvider } from "@/context/auth-provider";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <AuthProvider>{children}</AuthProvider>
         <Toaster richColors position="bottom-right" />
+        <Analytics />
       </body>
     </html>
   );
